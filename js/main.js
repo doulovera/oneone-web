9692db06882103172a040ceaf4c00fc12c46278c
Douglas Lovera <doulovera@gmail.com>
2021-04-09 00:15:14 -0500

    first new commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
- // Top section
-let serverSpan = document.getElementById("servers")
-let commandsSpan = document.getElementById("users")
+// Card section
+let serverSpan = document.getElementById("servers_number")
+let commandsSpan = document.getElementById("commands_number")
 
 // Command section
-let cmdsTable = document.getElementById('commands_table');
+// let cmdsTable = document.getElementById('commands_table');
 
 // Loading icon
-document.getElementById('loader').innerHTML = '<img src="https://s.svgbox.net/loaders.svg?ic=three-dots&fill=fff" width="32" height="32" draggable="false">'
+// document.getElementById('loader').innerHTML = '<img src="https://s.svgbox.net/loaders.svg?ic=three-dots&fill=fff" width="32" height="32" draggable="false">'
 
 const oneoneData = 'https://oneone-data.herokuapp.com/oneone'
 fetch(oneoneData)
@@ -16,16 +16,16 @@ fetch(oneoneData)
         serverSpan.innerHTML = res.guildSize
         commandsSpan.innerHTML = res.commandsSize
         
-        let cmds = res.commands;
+        // let cmds = res.commands;
 
-        Object.keys(cmds).forEach(key => {
-            let tr = document.createElement('tr');
-            tr.innerHTML = `
-                <th>${key}</th>
-                <td>${cmds[key]}</td>
-            `
-            cmdsTable.appendChild(tr)
-            document.getElementById('loader').innerHTML = '';
-        });
+        // Object.keys(cmds).forEach(key => {
+        //     let tr = document.createElement('tr');
+        //     tr.innerHTML = `
+        //         <th>${key}</th>
+        //         <td>${cmds[key]}</td>
+        //     `
+        //     cmdsTable.appendChild(tr)
+        //     document.getElementById('loader').innerHTML = '';
+        // });
     })
 
