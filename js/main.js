0804b72026094b51e47f70d5bb659b553957e08c
Douglas Lovera <doulovera@gmail.com>
2021-04-18 14:29:41 -0500

    Improved Commands section's code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
 // Card section
 const serverSpan = document.getElementById("servers_number");
 const commandsSpan = document.getElementById("commands_number");
-
-const cmdBotInfo = document.getElementById("cmd-bot-info");
-const cmdInfoInfo = document.getElementById("cmd-info-info");
-const cmdModInfo = document.getElementById("cmd-mod-info");
-const cmdFunInfo = document.getElementById("cmd-fun-info");
-
+// Command section
+const commandContainer = document.getElementsByClassName('main__commands-data--container');
 
 // Loading icon
 // document.getElementById('loader').innerHTML = '<img src="https://s.svgbox.net/loaders.svg?ic=three-dots&fill=fff" width="32" height="32" draggable="false">'
@@ -17,67 +13,23 @@ fetch(oneoneData)
     .then(data => {
 
         const { total, servers } = data;
-        const { bot, info, mod, fun } = data.commands;
+        // const { bot, info, mod, fun } = data.commands;
+        const cmds = data.commands;
 
         serverSpan.innerHTML = servers;
         commandsSpan.innerHTML = total;
+ 
+        const categories = ['bot', 'info', 'mod', 'fun'];
 
-        // console.log(bot);
-        // console.log(info);
-        // console.log(mod);
-        // console.log(fun);
-
-        bot.map(command => {
-            const article = document.createElement('article');
-            article.innerHTML = `
-                <p class="main__commands-data--title">${command.name}</p>
-                <p class="main__commands-data--body">${command.description}</p>
-            `;
-            article.className = 'main__commands-data-container';
-            cmdBotInfo.appendChild(article);
-        });
-
-        info.map(command => {
-            const article = document.createElement('article');
-            article.innerHTML = `
-                <p class="main__commands-data--title">${command.name}</p>
-                <p class="main__commands-data--body">${command.description}</p>
-            `;
-            article.className = 'main__commands-data-container';
-            cmdInfoInfo.appendChild(article);
-        });
-        
-        mod.map(command => {
-            const article = document.createElement('article');
-            article.innerHTML = `
-                <p class="main__commands-data--title">${command.name}</p>
-                <p class="main__commands-data--body">${command.description}</p>
-            `;
-            article.className = 'main__commands-data-container';
-            cmdModInfo.appendChild(article);
-        });
-        
-        fun.map(command => {
-            const article = document.createElement('article');
-            article.innerHTML = `
-                <p class="main__commands-data--title">${command.name}</p>
-                <p class="main__commands-data--body">${command.description}</p>
-            `;
-            article.className = 'main__commands-data-container';
-            cmdFunInfo.appendChild(article);
-        });
-        
-
-        
-        // let cmds = res.commands;
-
-        // Object.keys(cmds).forEach(key => {
-        //     let tr = document.createElement('tr');
-        //     tr.innerHTML = `
-        //         <th>${key}</th>
-        //         <td>${cmds[key]}</td>
-        //     `
-        //     commandsTable.appendChild(tr)
-        // });
-    })
+        for(let i = 0; i < commandContainer.length; i++) {
+            cmds[categories[i]].map(command => {
+                const article = document.createElement('article');
+                article.innerHTML = `
+                    <p class="main__commands-data--title">${command.name}</p>
+                    <p class="main__commands-data--body">${command.description}</p>
+                `;
+                article.className= "main__commands-data-container";
+                commandContainer[i].appendChild(article);
+            })
+}})
 
