948cf0fa366486ea9c8e69db87f57eefbaf2fdf4
Douglas Lovera <doulovera@gmail.com>
2021-09-09 18:47:41 -0500

    new Commit

---
+++ src/components/Server.ts
@@ -0,0 +1,7 @@
+import './Server.styles.css'
+
+export default function Server() {
+  return /*html*/`<section class="server__hero">
+    buenasd asd
+  </section>`
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import Server from './components/Server'
+
 import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = /*html*/`
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  ${Server()}
 `
