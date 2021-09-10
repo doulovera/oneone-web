a9c3f018beb1996d04d6d7d00f71bb78c730045d
Douglas Lovera <doulovera@gmail.com>
2021-09-09 19:41:41 -0500

    feat: Server Invite section done 👍

--- src/components/Server.ts
+++ src/components/Server.ts
@@ -2,6 +2,14 @@ import './Server.styles.css'
 
 export default function Server() {
   return /*html*/`<section class="server__hero">
-    buenasd asd
+    <div class="server__hero__image">
+      <h2 class="server__hero__title">
+        AWX
+      </h2>
+      <p class="server__hero__description">¡Únete a nuestro servidor de Discord!</p>
+      <a href="#" class="cta__button-outlined join-server">
+        Unirme
+      </a>
+    </div>
   </section>`
 }
